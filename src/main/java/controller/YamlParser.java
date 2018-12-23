1dc37c4ddf88e693236aec552d6ba8b2bbf55836
mariolarumlova <mariolarumlova@gmail.com>
2018-12-23 23:45:23 +0100

    #3 Writing to YAML file added

--- src/main/java/controller/YamlParser.java
+++ src/main/java/controller/YamlParser.java
@@ -1,17 +1,18 @@
+package controller;
+
 import model.Config;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Map;
 
 public class YamlParser {
 
     public static void main(String[] args) {
         try {
-            Config config = read("/config.yaml");
-            write(config);
+            Config config = read("/config_example.yaml");
+            write(config, "E:\\Projects\\wesvariantsgetter\\src\\main\\resources");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -25,14 +26,28 @@ public class YamlParser {
         }
     }
 
-    public static void write(Config config) {
+    public static String makeYaml(Config config) {
+//        TEMP
+//        Map<String, Object> rules = config.getRules();
+//        rules.put("mapper", "bwa");
+//        config.setRules(rules);
+//        TEMP
+
         Map<String, Map<String, Object>> map = config.getAsMap();
 
         DumperOptions options = new DumperOptions();
         options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
 
         Yaml yaml = new Yaml(options);
-        String output = yaml.dump(map);
-        System.out.println(output);
+        return yaml.dump(map);
+    }
+
+    public static void write(Config config, String path) {
+        String yamlToWrite = makeYaml(config);
+            try (PrintWriter out = new PrintWriter(path + "\\config.yaml")) {
+                out.println(yamlToWrite);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
     }
 }

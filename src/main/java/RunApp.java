229a19d0f2387df558679aef8b68f6528937f76b
mariolarumlova <mariolarumlova@gmail.com>
2018-12-25 21:54:47 +0100

    #15 It turns on, but then...

--- src/main/java/RunApp.java
+++ src/main/java/RunApp.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import settings.GuiHandler;
@@ -7,25 +8,26 @@ import settings.PreferencesManager;
 import settings.PropertiesGetter;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class RunApp extends Application {
 
     private static FXMLLoader loader;
 
-    private static volatile RunApp instance;
-
-    private RunApp() {}
-
-    public static RunApp getInstance() {
-        if (instance == null) {
-            synchronized (RunApp.class) {
-                if (instance == null) {
-                    instance = new RunApp();
-                }
-            }
-        }
-        return instance;
-    }
+//    private static volatile RunApp instance;
+//
+//    private RunApp() {}
+//
+//    public static RunApp getInstance() {
+//        if (instance == null) {
+//            synchronized (RunApp.class) {
+//                if (instance == null) {
+//                    instance = new RunApp();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
 
     public static void main(String[] args) {
         launch(args);
@@ -40,7 +42,7 @@ public class RunApp extends Application {
         String language = PreferencesManager.getInstance().getPreference("language", String.class);
         String name = firstUsage ? "Configuration" : "MainWindow";
         loader = new FXMLLoader();
-        Scene scene = GuiHandler.getInstance().getScene(loader, language, name);
+        Scene scene = getScene(loader, language, name);
         primaryStage.setScene(scene);
         primaryStage.show();
         } catch (IOException | PreferencesManager.UnsupportedTypeException | PreferencesManager.IncorrectKeyException e) {
@@ -54,5 +56,18 @@ public class RunApp extends Application {
 
     public static void setLoader(FXMLLoader loader) {
         RunApp.loader = loader;
+    }
+
+    public Scene getScene(FXMLLoader loader, String language, String name) throws IOException {
+        int[] sceneSize = GuiHandler.getInstance().getSceneSize();
+        int sceneWidth = sceneSize[0];
+        int sceneHeight = sceneSize[1];
+
+        loader.setResources(ResourceBundle.getBundle("labels_" + language));
+        Parent root = loader.load(getClass().getResourceAsStream("\\view\\" + name + ".fxml"));
+        Scene scene = new Scene(root, sceneWidth, sceneHeight);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("\\bootstrap3.css").toExternalForm());
+
+        return scene;
     }
 }

bb02f7ac90a8d8cd3cfc20049d9bb3fdf1d43ade
mariolarumlova <mariolarumlova@gmail.com>
2018-12-26 19:09:58 +0100

    #26 Configuration window layout improved

--- src/main/java/controller/ConfigurationController.java
+++ src/main/java/controller/ConfigurationController.java
@@ -1,9 +1,30 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import tools.PreferencesManager;
 
-public class ConfigurationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConfigurationController implements Initializable {
+
+    @FXML
+    Button firstUsageInstallButton;
+    @FXML
+    Button firstUsageSetupButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Image imageInstall = new Image(getClass().getResourceAsStream("/images/download.png"));
+        firstUsageInstallButton.setGraphic(new ImageView(imageInstall));
+        Image imageBrowse = new Image(getClass().getResourceAsStream("/images/browse.png"));
+        firstUsageSetupButton.setGraphic(new ImageView(imageBrowse));
+    }
 
     public void onInstallButtonPressed(ActionEvent actionEvent) {
         //TODO: Create a method
@@ -26,4 +47,6 @@ public class ConfigurationController {
             e.printStackTrace();
         }
     }
+
+
 }
